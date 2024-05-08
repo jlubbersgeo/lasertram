d72e5a4fa9552b5d2ab6b9b1c9799a7fb8c7b5aa
Lubbers, Jordan Edward <jlubbers@usgs.gov>
2024-05-08 00:07:49 +0000

    Update __init__.py [skip ci]

--- lasertram/__init__.py
+++ lasertram/__init__.py
@@ -2,6 +2,6 @@ from .calc import LaserCalc
 from .helpers import batch, conversions
 from .tram import LaserTRAM
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # __all__ = ["tram", "calc", "helpers"]
